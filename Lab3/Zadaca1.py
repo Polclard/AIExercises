0451f4821f15fdb3d4845d6e3ac7030be6f0b0b3
Polclard <alenjang007@gmail.com>
2023-03-29 00:34:37 +0200

    update Zadaca1.py update Zadaca2.py

--- Lab3/Zadaca1.py
+++ Lab3/Zadaca1.py
@@ -2,32 +2,31 @@ from constraint import *
 
 if __name__ == '__main__':
     problem = Problem()
-    
+
     # ---------------------------------------------------
-    problem.addVariable("Simona_prisustvo", [0,1])
-    problem.addVariable("Marija_prisustvo", [0,1])
-    problem.addVariable("Petar_prisustvo", [0,1])
+    problem.addVariable("Simona_prisustvo", [0, 1])
+    problem.addVariable("Marija_prisustvo", [0, 1])
+    problem.addVariable("Petar_prisustvo", [0, 1])
     problem.addVariable("vreme_sostanok", range(12, 20))
     # ----------------Add constrains here-----------------
-    
+
     problem.addConstraint(InSetConstraint([1]), ("Simona_prisustvo",))
-    problem.addConstraint(SomeInSetConstraint([1]), ("Marija_prisustvo","Petar_prisustvo"))
+    problem.addConstraint(SomeInSetConstraint([1]), ("Marija_prisustvo", "Petar_prisustvo"))
     # problem.addConstraint(InSetConstraint([13,14,16,19]), ("vreme_sostanok",))
 
-    problem.addConstraint(lambda m,p,v: (p == 0 and m == 1 and v in [14]) or (m == 0 and p == 1 and v in [13,16,19]), ( "Marija_prisustvo", "Petar_prisustvo", "vreme_sostanok"))
-
+    problem.addConstraint(
+        lambda m, p, v: (p == 0 and m == 1 and v in [14]) or (m == 0 and p == 1 and v in [13, 16, 19]),
+        ("Marija_prisustvo", "Petar_prisustvo", "vreme_sostanok"))
 
     prb = problem.getSolutions()
-    
+
     nov = dict()
-    
+
     for solution in prb:
         nov["Simona_prisustvo"] = solution["Simona_prisustvo"]
         nov["Marija_prisustvo"] = solution["Marija_prisustvo"]
         nov["Petar_prisustvo"] = solution["Petar_prisustvo"]
         nov["vreme_sostanok"] = solution["vreme_sostanok"]
-        print(nov) 
+        print(nov)
 
-    
-    
-    # [print(solution) for solution in problem.getSolutions()]
+        # [print(solution) for solution in problem.getSolutions()]
--- Lab3/Zadaca2.py
+++ Lab3/Zadaca2.py
@@ -1,14 +1,38 @@
 from constraint import *
 
+list_ai = []
+list_ml = []
+list_nlp = []
+
+
+counter_termin1 = 0
+counter_termin2 = 0
+def func(*args):
+
+    print(args)
+
+    list_ai.append(args[0])
+    list_ml.append(args[1])
+    list_nlp.append(args[2])
+
+    # print(f"AI: {len(list_ai}\nML: {list_ml} \nNLP: {list_nlp}\n")
+    problem.addConstraint(AllEqualConstraint(),  ("AI",))
+    problem.addConstraint(AllEqualConstraint(),  ("ML",))
+    problem.addConstraint(AllEqualConstraint(),  ("NLP",))
+    return True
+
+
+    # return len(list_ai) <= 4 and len(list_ml) <= 4 and len(list_nlp) <= 4
+
 if __name__ == '__main__':
     num = int(input())
-    
+
     papers = dict()
-    
+
     aiNum = 0
     mlNum = 0
-    nlpNum = 0    
-    
+    nlpNum = 0
+
     paper_info = input()
     while paper_info != 'end':
         title, topic = paper_info.split(' ')
@@ -20,28 +44,29 @@ if __name__ == '__main__':
             nlpNum+=1
         papers[title] = topic
         paper_info = input()
-        
+
     #---------------print------------------------
     # print(f"{aiNum} - {mlNum} - {nlpNum}")
     #--------------------------------------------
-    
+
     # Tuka definirajte gi promenlivite
-    
+
     domain = [f'T{i + 1}' for i in range(num)]
-    
-    problem = Problem(BacktrackingSolver()) 
-    
+
+    problem = Problem(BacktrackingSolver())
+
     # Dokolku vi e potrebno moze da go promenite delot za dodavanje na promenlivite
     problem.addVariables(("AI","ML","NLP"), domain)
-    
-    # Tuka dodadete gi ogranichuvanjata
-    if(aiNum <= 4 and mlNum <= 4 and nlpNum <= 4):
-        problem.addConstraint(AllDifferentConstraint(), ("AI","ML","NLP"))
 
-        
+    # Tuka dodadete gi ogranichuvanjata
+    if(aiNum <= 4 or mlNum <= 4 or nlpNum <= 4):
+        problem.addConstraint(AllDifferentConstraint(), ("AI", "ML", "NLP"))
+    else:
+        problem.addConstraint(func, ())
+
     result = problem.getSolution()
+
+    print(result)
 
     for key,value in papers.items():
         print(f"{key} ({value}): {result[value]}")
-
-
